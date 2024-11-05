9f0b5878317f8137f2a55a7646aeb53957e11af6
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-05 23:27:05 +0100

    H2 Database configurated and data.sql created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO shops (id,name,owner,utr) VALUES (default,'Mediadings - Computers & more','Hans Dings',1234567890,);
+
+INSERT INTO computers (id,brand,memory,processor, operating_system, price, shop_id) VALUES (default, 'HP', 64, 'Intel Core i7-12700K', 800, 1);
+INSERT INTO computers (id,brand,memory,processor, operating_system, price, shop_id) VALUES (default, 'Dell', 16, 'Intel Core i5-12400F', 700, 1);
+INSERT INTO computers (id,brand,memory,processor, operating_system, price, shop_id) VALUES (default, 'Acer', 32, 'AMD Ryzen 7 5800X', 1);
+INSERT INTO computers (id,brand,memory,processor, operating_system, price, shop_id) VALUES (default, 'Lenovo', 32, 'Qualcomm Snapdragon 8 Gen 2', 1);
+INSERT INTO computers (id,brand,memory,processor, operating_system, price, shop_id) VALUES (default, 'Acme', 8, 'ARM Cortex-A76', 1);
