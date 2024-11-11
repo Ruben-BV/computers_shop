61a25e75a30eb381df038a40c72ab4fac1e9aa32
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-11 23:48:53 +0100

    Tests Shop- and ComputerService

--- src/test/java/org/factoriaf5/computers_shop/services/ComputerServiceTest.java
+++ src/test/java/org/factoriaf5/computers_shop/services/ComputerServiceTest.java
@@ -1,5 +1,69 @@
 package org.factoriaf5.computers_shop.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.factoriaf5.computers_shop.models.Computer;
+import org.factoriaf5.computers_shop.models.Shop;
+import org.factoriaf5.computers_shop.repositories.ComputerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ComputerServiceTest {
+
+
+    @Mock
+    private ComputerRepository computerRepository;
+    @InjectMocks
+    ComputerService computerService;
+    @Mock
+    private Computer mockComputer1;
+    @Mock
+    private Computer mockComputer2;
+    
+    
+    @BeforeEach
+    void setUp() {
+        
+        this.mockComputer1 = new Computer(1L, "BrandA", 8, "ProcessorA", "OS1", 1000, new Shop(1L, "ShopA", "OwnerA", 123L));
+        this.mockComputer2 = new Computer(2L, "BrandB", 16, "ProcessorB", "OS2", 1250, new Shop(1L, "ShopA", "OwnerA", 123L));
+
+    }
+    
+    @Test
+    void testAddComputer() {
+        
+    }
+
+    @Test
+    void testDeleteComputerByBrand() {
+        
+    }
+
+    @Test
+    void testFindComputerByBrand() {
+        
+    }
+
+    @Test
+    void testGetAll() {
+        List<Computer> mockComputers = new ArrayList<>();
+        
+        mockComputers.add(mockComputer1);
+        mockComputers.add(mockComputer2);
+        
+        when(computerRepository.findAll()).thenReturn(mockComputers);
+        
+        List<Computer> result = computerService.getAll();
+        assertTrue(result.equals(mockComputers));
+    }
     
 }
--- src/test/java/org/factoriaf5/computers_shop/services/ShopServiceTest.java
+++ src/test/java/org/factoriaf5/computers_shop/services/ShopServiceTest.java
@@ -1,5 +1,52 @@
 package org.factoriaf5.computers_shop.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.factoriaf5.computers_shop.models.Shop;
+import org.factoriaf5.computers_shop.repositories.ShopRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ShopServiceTest {
+    
+    @Mock
+    private ShopRepository shopRepository;
+    @InjectMocks
+    ShopService shopService;
+    @Mock
+    private Shop mockShop1;
+    @Mock
+    private Shop mockShop2;
+    
+    
+    @BeforeEach
+    void setUp() {
+    
+        this.mockShop1 = new Shop(1L, "ShopA", "OwnerA", 123L);
+        this.mockShop2 = new Shop(2L, "ShopB", "OwnerB", 234L);
+    }
+    
+    @Test
+    void testGetAll() {
+        
+        List<Shop> mockShops = new ArrayList<>();
+        
+        mockShops.add(mockShop1);
+        mockShops.add(mockShop2);
+        
+        when(shopRepository.findAll()).thenReturn(mockShops);
+        
+        List<Shop> result = shopService.getAll();
+        assertTrue(result.equals(mockShops));
+    }
     
 }
